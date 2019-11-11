3009131b9efa81916cce57d68191a8adb615d940
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-11 02:14:10 -0500

    tweet

--- index.php
+++ index.php
@@ -24,23 +24,38 @@ if (!isset($_SESSION['access_token'])) {
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
     $content = $connection->get("account/verify_credentials");
-    $statuses = $connection->get("statuses/home_timeline", ["screen_name"=>$content->screen_name, "count"=>9]);
+    $statuses = $connection->get("statuses/home_timeline", ["screen_name" => $content->screen_name, "count" => 9]);
     //$contentArr = json_decode($content,true);
 
-foreach($statuses as $key => $status){
-    $arrStatus=[];
-    $arrStatus['created_at'] = $status->created_at;
-    $arrStatus['created_by'] = $status->name;
-    $arrStatus['message'] = $status->text;
-    $arrStatus['pp'] = $status->user->profile_image_url;
-    $arrStatus['uname'] = $status->user->name;
-    $arrStatuses[] = $arrStatus;
-}
+    foreach ($statuses as $key => $status) {
+        $arrStatus = [];
+        $arrStatus['created_at'] = $status->created_at;
+        $arrStatus['created_by'] = $status->name;
+        $arrStatus['message'] = $status->text;
+        $arrStatus['pp'] = $status->user->profile_image_url;
+        $arrStatus['uname'] = $status->user->name;
+        $arrStatuses[] = $arrStatus;
+    }
 
     $pimage = $content->profile_image_url;
     $pname = $content->name;
+}
 
 
+if (isset($_POST['tweet'])) {
+    $mytweet = $_POST['tweetmsg'];
+    $tweeting = $connection->post("statuses/update", ["status" => $mytweet]);
+    if ($connection->getLastHttpCode() == 200) {
+        //tweet sent successfully
+        echo '<script language="javascript">';
+        echo 'alert("Tweet posted successfully")';
+        echo '</script>';
+    } else {
+        // tweet failed
+        echo '<script language="javascript">';
+        echo 'alert("Tweet Failed.")';
+        echo '</script>';
+    }
 }
 ?>
 
@@ -64,39 +79,41 @@ foreach($statuses as $key => $status){
             </div>
             <div class="row">
                 <div class="col col-6 mx-auto mt-3">
-                    <div class="input-group">
-                        <div class="input-group-prepend"></div><input class="form-control" type="text" placeholder="Need to say something?">
-                        <div class="input-group-append"><button class="btn btn-primary" type="button">Tweet</button></div>
-                    </div>
+                    <form method="post" action=''>
+                        <div class="input-group">
+                            <div class="input-group-prepend"></div><input class="form-control" type="text" placeholder="Need to say something?" name="tweetmsg">
+                            <div class="input-group-append"><button class="btn btn-primary" type="submit" name="tweet">Tweet</button></div>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div class="row people">
 
-                <?php 
-                    foreach ($arrStatuses as $key => $arrStatus){
-                        echo("
+                <?php
+                foreach ($arrStatuses as $key => $arrStatus) {
+                    echo ("
                         <div class='col-md-6 col-lg-4 item'>
                         <div class='box'>
                             <p class='description'>" . $arrStatus['message'] . "</p>
                         </div>
-                        <div class='author'><img class='rounded-circle' src='". $arrStatus['pp'] . "'>
-                            <h5 class='name'>". $arrStatus['uname'] . "</h5>
+                        <div class='author'><img class='rounded-circle' src='" . $arrStatus['pp'] . "'>
+                            <h5 class='name'>" . $arrStatus['uname'] . "</h5>
                             <p class='title'> " . $arrStatus['created_at'] . "</p>
                         </div>
                     </div>
                         
                         
                         ");
-                    }
-                
+                }
+
                 ?>
 
             </div>
         </div>
 
-        
+
     </div>
-    
+
 
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
