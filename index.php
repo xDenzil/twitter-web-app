a710aadddb13b646c564dd7b1d4f06c1992cac2e
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 22:45:42 -0500

    stat

--- index.php
+++ index.php
@@ -19,12 +19,13 @@ if (!isset($_SESSION['access_token'])) {
     $url = $connection->url('oauth/authorize', array('oauth_token' => $request_token['oauth_token'])); //Send to Twitter's Authorization page
     header('Location: ' . $url);
 } else { 
-    echo('hello');
+    //echo('hello');
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
     $content = $connection->get("account/verify_credentials");
     echo "<pre>";
     echo($content);
-    echo "<pre>";
+    echo "</pre>";
+    echo $contennt->status->text;
     
 }
