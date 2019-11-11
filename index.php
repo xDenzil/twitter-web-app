8477460510090c4450a7fb6ee9c01e0f352a00b2
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 22:52:23 -0500

    works

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (!isset($_SESSION['access_token'])) {
     //echo('hello');
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-    $content = $connection->get("account/verify_credentials");
+    $content = $connection->get("statuses/user_timeline", ["count" => 25, "exclude_replies" => true]);
     echo $content->status->text;
 
     echo "<pre>";
