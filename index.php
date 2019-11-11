24bdc0d3ce16dbbee0ea506845eb94ae14b80150
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 22:10:46 -0500

    ech

--- index.php
+++ index.php
@@ -22,7 +22,9 @@ if (!isset($_SESSION['access_token'])) {
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
     $statuses = $connection->get('statuses/home_timeline", ["count" => 25, "exclude_replies" => true]');
-    echo($statuses);
+    echo "<pre>";
+    print_r($statuses);
+    echo "string";
     //echo($user);
     
 }
