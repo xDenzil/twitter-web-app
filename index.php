f56432755072cb7c49804ee90b3027aa6e51c5ce
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 21:36:45 -0500

    user

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['access_token'])) {
     echo('hello');
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-    $user = $connection->get('account/verify_credentials', ['tweet_mode' => 'extended', 'include_entities' => 'true']);
+    $user = $connection->get('account/verify_credentials');
     echo $user->status->text;
     //echo($user);
     
