13dc9ae7fb0412546cb6d8db08eb9cbc6d5feded
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 21:51:39 -0500

    stat

--- callback.php
+++ callback.php
@@ -16,7 +16,6 @@ $request_token['oauth_token_secret'] = $_SESSION['oauth_token_secret']; //Get te
 if (isset($_REQUEST['oauth_token']) && $request_token['oauth_token'] !== $_REQUEST['oauth_token']) {
     // Abort! Something is wrong.
     echo('error. try again.');
-    header('Location: www.google.com');
 } else {
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $request_token['oauth_token'], $request_token['oauth_token_secret']);
     $access_token = $connection->oauth("oauth/access_token", ["oauth_verifier" => $_REQUEST['oauth_verifier']]);
--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION['access_token'])) {
     echo('hello');
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
-    $user = $connection->get('account/verify_credentials');
+    $user = $connection->get('statuses/home_timeline", ["count" => 25, "exclude_replies" => true]');
     echo $user->status->text;
     //echo($user);
     
