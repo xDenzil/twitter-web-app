15730793f7442ec178eb7f8a7ef15d0269ddbe13
Denzil Williams <denzilwilliams@Denzils-MacBook-Pro.local>
2019-11-10 23:15:05 -0500

    rat

--- index.php
+++ index.php
@@ -23,11 +23,11 @@ if (!isset($_SESSION['access_token'])) {
     $access_token = $_SESSION['access_token'];
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $access_token['oauth_token'], $access_token['oauth_token_secret']);
     $content = $connection->get("statuses/user_timeline", ["count" => 25, "exclude_replies" => true]);
-    echo $content->status->text;
+    //echo $content->status->text;
 
-    echo "<pre>";
-    print_r($content);
-    echo "<pre>";
+    //echo "<pre>";
+    //print_r($content);
+    //echo "<pre>";
     
 }?>
 
